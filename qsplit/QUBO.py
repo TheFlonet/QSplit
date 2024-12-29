fab6ca873ae5fc5abaa468ee123b3a37513bdd2a
Mario Bifulco <48624954+TheFlonet@users.noreply.github.com>
2024-12-29 12:37:29 +0100

    Improved QUBO to handle problems of any size

--- qsplit/QUBO.py
+++ qsplit/QUBO.py
@@ -9,14 +9,22 @@ class QUBO:
     def __init__(self, qubo_dict: QUBO_DICT, cols_idx: List[int] | None = None, 
                  rows_idx: List[int] | None = None, to_transform: bool = True):
         self.qubo_dict: QUBO_DICT = qubo_dict
+
         if cols_idx is None and rows_idx is None:
             raise ValueError('QUBO class requires at least one of cols_idx or rows_idx to be not None')
         self.cols_idx: List[int] = cols_idx if cols_idx is not None else rows_idx
         self.rows_idx: List[int] = rows_idx if rows_idx is not None else cols_idx
+        if not (len(self.cols_idx) % 2 == 0):
+            self.cols_idx.append(self.cols_idx[-1] + 1)
+        if not (len(self.rows_idx) % 2 == 0):
+            self.rows_idx.append(self.rows_idx[-1] + 1)
+        assert len(self.cols_idx) == len(self.rows_idx), 'cols_idx and rows_idx must have the same length, QUBO problem must be square'
+        self.problem_size: int = len(self.cols_idx)
+
         self.solutions: pd.DataFrame | None = None
         self.qubo_matrix: np.ndarray | None = None
         self.__to_transform: bool = to_transform
-        self.__from_dict_to_matrix((len(self.rows_idx), len(self.cols_idx)))
+        self.__from_dict_to_matrix()
 
     def __is_upper_triangular(self) -> bool:
         if self.qubo_matrix is None:
@@ -27,10 +35,10 @@ class QUBO:
 
         return True
 
-    def __from_dict_to_matrix(self, dims: Tuple[int, int]) -> None:
-        self.qubo_matrix = np.zeros(dims)
+    def __from_dict_to_matrix(self) -> None:
+        self.qubo_matrix = np.zeros((self.problem_size, self.problem_size))
         for k, v in self.qubo_dict.items():
-            self.qubo_matrix[k[0] % dims[0], k[1] % dims[1]] = v
+            self.qubo_matrix[k[0] % self.problem_size, k[1] % self.problem_size] = v
         if not self.__to_transform:
             return 
         if self.__is_upper_triangular():
